ba981a16d2cd69deeb9e6db37a793c797cb9ed55
Nastasia Demont <nastasia@demont.me>
2021-06-07 18:51:09 +0200

    Cointainer for wordpress setting ok

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -1,6 +1,13 @@
+#mysql
+service mysql start
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
 #phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
 tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz
+mkdir -p /var/www/html
 mv phpMyAdmin-4.9.7-all-languages /var/www/html/phpmyadmin
 
 #wordpress
@@ -17,3 +24,5 @@ openssl req -x509 -out server.crt -keyout server.key \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 cd /
+
+bash
