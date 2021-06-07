fcc2bfb7f5b5ab3e4c7ad21b3bb1139f6ecb4345
Nastasia Demont <nastasia@demont.me>
2021-06-07 17:52:20 +0200

    Edit mariadb Dockerfile

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -6,6 +6,14 @@ mv phpMyAdmin-4.9.7-all-languages /var/www/html/phpmyadmin
 #wordpress
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
-chmod 765 wordpress
+chmod 755 wordpress
 mv wordpress /var/www/html
 mv wordpress_config.php /var/www/html/wordpress/wp-config.php
+
+#sll
+cd /etc/ssl
+openssl req -x509 -out server.crt -keyout server.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+  printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+cd /
