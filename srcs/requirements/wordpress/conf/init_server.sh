ca09fbfd014ea2374e5731ce07db16913fdfff23
Nastasia Demont <nastasia@demont.me>
2021-06-07 17:40:52 +0200

    Add php

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -1,3 +1,8 @@
+#phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
+tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz
+mv phpMyAdmin-4.9.7-all-languages /var/www/html/phpmyadmin
+
 #wordpress
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
