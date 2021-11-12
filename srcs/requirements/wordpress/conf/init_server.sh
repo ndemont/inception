f1a776a8df0574d0b35107ea39757edfde0ef068
Nastasia Demont <ndemont@student.42.fr>
2021-11-12 11:18:13 +0100

    Update with version 3 of compose

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -24,5 +24,3 @@ openssl req -x509 -out server.crt -keyout server.key \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 cd /
-
-bash
