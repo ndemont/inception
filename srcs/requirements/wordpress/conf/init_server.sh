c054807fcebb2b770da312531908f77086a0808a
Nastasia Demont <nastasia@demont.me>
2021-11-30 21:23:46 +0100

    Add command to launch php

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -22,3 +22,5 @@ openssl req -x509 -out server.crt -keyout server.key \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 cd /
+
+/usr/sbin/php-fmp7.3
