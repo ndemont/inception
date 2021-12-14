4588246888eb528ccd47dc54b1dac4304720c2d6
Nastasia Demont <nastasia@demont.me>
2021-12-14 17:20:26 +0100

    Update php

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -4,6 +4,10 @@ echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_
 echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
+#php
+mkdir /run/php
+mv php-fpm.conf /etc/php/7.3/fpm
+
 #phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
 tar xvf phpMyAdmin-4.9.7-all-languages.tar.gz
@@ -23,4 +27,4 @@ openssl req -x509 -out server.crt -keyout server.key \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 cd /
 
-/usr/sbin/php-fmp7.3
+/usr/sbin/php-fpm7.3
