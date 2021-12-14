34f35103639b56632e3b72da4a3e8965889be986
Nastasia Demont <nastasia@demont.me>
2021-12-14 18:56:54 +0100

    Trying to get wp files in ngiinx

--- srcs/requirements/nginx/conf/init_server.sh
+++ srcs/requirements/nginx/conf/init_server.sh
@@ -5,7 +5,6 @@ openssl req -x509 -out server.crt -keyout server.key \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 cd /
+cp -R data/wordpress /var/www/html
 
-bash
-
-#nginx -g 'daemon off;'
+nginx -g 'daemon off;'
