d5358f1b61217eb0d71f5315e509e5f5f8a80475
Nastasia Demont <nastasia@demont.me>
2022-03-30 11:55:03 +0200

    Finaluy resolve socker pb for database -> use tcp instead

--- srcs/requirements/mariadb/conf/init_server.sh
+++ srcs/requirements/mariadb/conf/init_server.sh
@@ -2,10 +2,10 @@
 
 service mysql start
 
-echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql
-echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';" | mysql
+echo "CREATE DATABASE $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
-echo "USE wordpress;" | mysql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql
 
 service mysql stop
 
