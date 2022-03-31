0400dd0d6cc15163b8bc445d95707a589349d402
Nastasia Demont <nastasia@demont.me>
2022-03-31 17:50:04 +0200

    Edit wp container

--- srcs/requirements/wordpress/conf/init_server.sh
+++ srcs/requirements/wordpress/conf/init_server.sh
@@ -1,21 +1,15 @@
-#php
+set -vxe
+#php set up
 mkdir /run/php
-echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
-#wordpress
-mkdir -p /var/www/html
-mv wordpress /var/www/html
-mv wordpress_config.php /var/www/html/wordpress/wp-config.php
+mv www.conf /etc/php/7.3/fpm/pool.d/www.conf
 
+#wordpress set up
+mkdir -p /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz -C /var/www/html/
+#mv wordpress /var/www/html
+mv wp-config.php /var/www/html/wordpress/wp-config.php
 
-#sll
-cd /etc/ssl
-openssl req -x509 -out server.crt -keyout server.key \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=localhost' -extensions EXT -config <( \
-  printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-cd /
 
+#execution
 /usr/sbin/php-fpm7.3 -F
-
