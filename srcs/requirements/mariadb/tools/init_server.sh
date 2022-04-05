65b60992afb249e954df9082f923df76cfdf83b1
ndemont <nastasia@demont.me>
2022-04-05 20:04:07 +0200

    LOL

--- srcs/requirements/mariadb/tools/init_server.sh
+++ srcs/requirements/mariadb/tools/init_server.sh
@@ -2,13 +2,14 @@
 
 set -xve
 
-mysqld_safe --skip-grant-tables --skip-networking &
-
 service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql 
 
 service mysql stop
+
+mysqld --user=mysql
