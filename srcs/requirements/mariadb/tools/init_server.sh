d884b5457ea6d6370f38bdc33f134ace637b72df
ndemont <nastasia@demont.me>
2022-04-13 13:00:03 +0200

    Persistence & wordpress install automatised

--- srcs/requirements/mariadb/tools/init_server.sh
+++ srcs/requirements/mariadb/tools/init_server.sh
@@ -2,14 +2,18 @@
 
 set -xve
 
-service mysql start
+FILE=/var/lib/mysql/.db_bootstrap
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql
-
-service mysql stop 
+if [ ! -f "$FILE" ]
+then
+    service mysql start
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
+    echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+    echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
+    echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql
+    service mysql stop 
+    touch /var/lib/mysql/.db_bootstrap
+fi
 
 exec mysqld -u mysql
--- srcs/requirements/wordpress/tools/init_server.sh
+++ srcs/requirements/wordpress/tools/init_server.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+set -xve
+
+rm /var/www/html/wordpress/wp-config.php
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path="$WP_PATH" --allow-root --skip-check
+wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --path="$WP_PATH" --allow-root
+
 /usr/sbin/php-fpm7.3 -F
